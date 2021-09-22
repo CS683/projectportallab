c19ba0bc2cd8e33d67de381a1c597d769dd261a0
Yuting <yuditzh@gmail.com>
2021-09-22 16:00:08 -0400

    Using intents for activity communication.

--- ProjectPortalLabUseActivity/app/src/main/java/edu/bu/projectportal/MainActivity.kt
+++ ProjectPortalLabUseActivity/app/src/main/java/edu/bu/projectportal/MainActivity.kt
@@ -1,30 +1,31 @@
 package edu.bu.projectportal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity(), EditProjectListener {
+class MainActivity : AppCompatActivity() {
+    private lateinit var projTitle: TextView
+    private lateinit var projDesc: TextView
+    private lateinit var editProj: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, DetailFragment.newInstance())
-                .commitNow()
+
+        projTitle = findViewById(R.id.projTitle)
+        projDesc =  findViewById(R.id.projDesc)
+
+        editProj = findViewById(R.id.editProj)
+
+        projTitle.text =  Project.project.title
+        projDesc.text = Project.project.description
+
+        editProj.setOnClickListener {
+            startActivity(Intent(this, EditProjectActivity::class.java))
         }
     }
 
-    override fun editProj(){
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, EditFragment.newInstance())
-            .commitNow()
-
-    }
-
-    override fun editProjDone(){
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, DetailFragment.newInstance())
-            .commitNow()
-    }
 }
